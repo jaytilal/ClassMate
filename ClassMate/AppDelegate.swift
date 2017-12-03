f4a897d43cbc59eb17765c7275fbed3488c2efa8
Jayti Lal <jayti@Jaytis-MacBook-Pro.local>
2017-12-03 15:05:43 -0800

    User Signup

--- ClassMate/AppDelegate.swift
+++ ClassMate/AppDelegate.swift
@@ -8,16 +8,18 @@
 
 import UIKit
 import CoreData
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
-        return true
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?)
+        -> Bool {
+            FirebaseApp.configure()
+            return true
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
---
+++ ClassMate/SignupViewController.swift
@@ -0,0 +1,67 @@
+//
+//  SignupViewController.swift
+//  ClassMate
+//
+//  Created by Jayti Lal on 12/3/17.
+//  Copyright © 2017 Jayti Lal. All rights reserved.
+//
+
+import UIKit
+import Firebase
+import FirebaseAuth
+class SignupViewController: UIViewController {
+
+    @IBOutlet weak var EmailId: UITextField!
+    @IBOutlet weak var Password: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    @IBOutlet weak var Submit: UIButton!
+    @IBAction func SignUp(_ sender: UIButton) {
+        if self.EmailId.text == "" {
+            let alertController = UIAlertController(title: "Error", message: "Please enter your email and password", preferredStyle: .alert)
+            
+            let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+            alertController.addAction(defaultAction)
+            
+            present(alertController, animated: true, completion: nil)
+            
+            
+        } else {
+            Auth.auth().createUser(withEmail: EmailId.text!, password: Password.text!) { (user, error) in
+                
+                if error == nil {
+                    print("You have successfully signed up")
+                
+                    
+                } else {
+                    let alertController = UIAlertController(title: "Error", message: error?.localizedDescription, preferredStyle: .alert)
+                    
+                    let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+                    alertController.addAction(defaultAction)
+                    
+                    self.present(alertController, animated: true, completion: nil)
+                }
+            }
+        }
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
