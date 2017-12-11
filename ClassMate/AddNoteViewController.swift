f585a9e6a3f9d9f508c825f6b1ab222d775439ac
Jayti Lal <jayti@Jaytis-MacBook-Pro.local>
2017-12-11 14:08:18 -0800

    Add images to firebase and storage

--- ClassMate/AddNoteViewController.swift
+++ ClassMate/AddNoteViewController.swift
@@ -11,14 +11,24 @@ import Firebase
 import FirebaseDatabase
 import FirebaseStorage
 
+
+struct Constants {
+    
+    struct Images {
+        static let imagesFolder: String = "images"
+    }
+    
+}
+
 class AddNoteViewController: UIViewController {
     @IBOutlet weak var noteTopic: UITextField!
     @IBOutlet weak var noteDescription: UITextField!
     let databaseRef = Database.database().reference()
+    let storageRef = Storage.storage().reference()
     let user = (Auth.auth().currentUser?.email)!
     var groupId : String!
     @IBOutlet weak var noteContents: UITextView!
-    
+    var downloadURL = ""
     @IBAction func Back(_ sender: UIBarButtonItem) {
         if let nav = self.navigationController {
             nav.popViewController(animated: true)
@@ -27,20 +37,67 @@ class AddNoteViewController: UIViewController {
             self.dismiss(animated: true, completion: nil)
         }
     }
+    
     func addNote(){
         print("inside add note")
         print(groupId)
         let reference = databaseRef.child("groups").child(groupId).child("notes")
         let key = reference.childByAutoId().key;
+        
+        
         let note = ["id":key,
                     "topic": noteTopic.text!,
                     "description": noteDescription.text!,
-                    "content": noteContents.text!
+                    "content": noteContents.text!,
+                    "downloadURL" : downloadURL
         ]
       
         reference.child(key).setValue(note)
         
     }
+    func uploadImage(_ image: UIImage, progressBlock: @escaping (_ percentage: Double) -> Void, completionBlock: @escaping (_ url: URL?, _ errorMessage: String?) -> Void) {
+        let storage = Storage.storage()
+        let storageReference = storage.reference()
+        
+        // storage/carImages/image.jpg
+        let imageName = "\temp.jpg"
+        let imagesReference = storageReference.child(Constants.Images.imagesFolder).child(imageName)
+        
+        if let imageData = UIImageJPEGRepresentation(image, 0.8) {
+            let metadata = StorageMetadata()
+            metadata.contentType = "image/jpeg"
+            
+            let uploadTask = imagesReference.putData(imageData, metadata: metadata, completion: { (metadata, error) in
+                if let metadata = metadata {
+                    completionBlock(metadata.downloadURL(), nil)
+                } else {
+                    completionBlock(nil, error?.localizedDescription)
+                }
+            })
+            uploadTask.observe(.progress, handler: { (snapshot) in
+                guard let progress = snapshot.progress else {
+                    return
+                }
+                
+                let percentage = (Double(progress.completedUnitCount) / Double(progress.totalUnitCount)) * 100
+                progressBlock(percentage)
+            })
+        } else {
+            completionBlock(nil, "Image couldn't be converted to Data.")
+        }
+    }
+    
+
+    
+    @IBAction func uploadAttachment(_ sender: UIButton) {
+        let myPickerController = UIImagePickerController()
+        myPickerController.delegate = self;
+        myPickerController.sourceType =  UIImagePickerControllerSourceType.photoLibrary
+        self.present(myPickerController, animated: true, completion: nil)
+        
+        print(self.downloadURL)
+    }
+    
     @IBAction func addNoteToDB(_ sender: UIButton) {
         addNote()
         print("note added to Database")
@@ -59,3 +116,30 @@ class AddNoteViewController: UIViewController {
     }
     
 }
+extension AddNoteViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            picker.dismiss(animated: true, completion: nil)
+            print("Image Selected")
+            self.uploadImage(image, progressBlock: { (percentage) in
+                print(percentage)
+            }, completionBlock: { [weak self] (fileURL, errorMessage) in
+                guard self != nil else {
+                    return
+                }
+                self?.downloadURL = (fileURL!.absoluteString)
+                print((fileURL?.absoluteString)!)
+                print(errorMessage ?? "No error")
+                
+            })
+        }
+    }
+    
+}
+
+
