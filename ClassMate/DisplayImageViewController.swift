526880650d1040c8d60abc433de35634939e5f77
Jayti Lal <jayti@Jaytis-MacBook-Pro.local>
2017-12-12 22:55:55 -0800

    Add download of images to main thread

--- ClassMate/DisplayImageViewController.swift
+++ ClassMate/DisplayImageViewController.swift
@@ -12,10 +12,13 @@ class DisplayImageViewController: UIViewController {
 
     @IBOutlet weak var Image: UIImageView!
     var Url = ""
+    var queue = DispatchQueue(label: "responseQueue", qos: .utility)
     override func viewDidLoad() {
         super.viewDidLoad()
         if Url != ""{
-            getImage(url: Url)
+            //getImage(url: Url)
+            Image.imageFromServerURL(urlString: Url)
+            reloadInputViews()
             print("Done")
         }
         // Do any additional setup after loading the view.
@@ -25,30 +28,20 @@ class DisplayImageViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    func getImage(url : String) {
-        let PictureURL = URL(string: url)!
-        
-        let session = URLSession(configuration: .default)
-        var image = UIImage()
-        let downloadPicTask = session.dataTask(with:PictureURL) { (data, response, error) in
-            if let e = error {
-                print("Error downloading cat picture: \(e)")
-            } else {
-                if let res = response as? HTTPURLResponse {
-                    print("Downloaded  picture with response code \(res.statusCode)")
-                    if let imageData = data {
-                        image = UIImage(data: imageData)!
-                        self.Image.image = image
-                    } else {
-                        print("Couldn't get image: Image is nil")
-                    }
-                } else {
-                    print("Couldn't get response code for some reason")
-                }
-            }
-        }
-        
-        downloadPicTask.resume()
-    }
+    
     
 }
+extension UIImageView {
+    public func imageFromServerURL(urlString: String) {
+        URLSession.shared.dataTask(with: NSURL(string: urlString)! as URL, completionHandler: { (data, response, error) -> Void in
+            if error != nil {
+                print(error)
+                return
+            }
+            DispatchQueue.main.async(execute: { () -> Void in
+                let image = UIImage(data: data!)
+                self.image = image
+            })
+            
+        }).resume()
+    }}
--- ClassMate/ShowNoteViewController.swift
+++ ClassMate/ShowNoteViewController.swift
@@ -26,34 +26,9 @@ class ShowNoteViewController: UIViewController,UICollectionViewDelegate,UICollec
         Content.text = DisplayNote.noteContent
         Description.text = DisplayNote.noteDesc
         url = DisplayNote.downloadUrl
-
+        self.thumbnails.reloadSections(IndexSet(integer : 0))
     }
     
-    func getImage(url : String) -> UIImage{
-        let PictureURL = URL(string: url)!
-    
-        let session = URLSession(configuration: .default)
-        var image = UIImage()
-       let downloadPicTask = session.dataTask(with:PictureURL) { (data, response, error) in
-            if let e = error {
-                print("Error downloading cat picture: \(e)")
-            } else {
-                if let res = response as? HTTPURLResponse {
-                    print("Downloaded  picture with response code \(res.statusCode)")
-                    if let imageData = data {
-                        image = UIImage(data: imageData)!
-                    } else {
-                        print("Couldn't get image: Image is nil")
-                    }
-                } else {
-                    print("Couldn't get response code for some reason")
-                }
-            }
-        }
-        downloadPicTask.resume()
-        return image
-       
-    }
    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -70,37 +45,14 @@ class ShowNoteViewController: UIViewController,UICollectionViewDelegate,UICollec
     }
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         // get a reference to our storyboard cell
-         let session = URLSession(configuration: .default)
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseidentifier, for: indexPath as IndexPath) as! ImageCollectionViewCell
         cell.downloadUrl = self.url[indexPath.item]
+        cell.ImageThumbnail.imageFromServerURL(urlString: cell.downloadUrl)
         
-        let task = session.dataTask(with: URL(string:cell.downloadUrl)!)
-        { (data, response, error) in
-            if let e = error {
-                print("Error downloading picture: \(e)")
-            } else {
-                if let res = response as? HTTPURLResponse {
-                    print("Downloaded  picture with response code \(res.statusCode)")
-                    if let imageData = data {
-                        cell.ImageThumbnail.image = UIImage(data: imageData)!
-                    } else {
-                        print("Couldn't get image: Image is nil")
-                    }
-                } else {
-                    print("Couldn't get response code for some reason")
-                }
-            }
-        }
-        
-        task.resume()
-        self.thumbnails.reloadSections(IndexSet(integer : 0))
 
         return cell
     }
     
-    override func viewDidAppear(_ animated: Bool) {
-        self.thumbnails.reloadSections(IndexSet(integer : 0))
-    }
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return self.url.count
     }
